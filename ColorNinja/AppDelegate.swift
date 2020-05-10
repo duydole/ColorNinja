e93c2e90945456d0aeaebb13ac6d3ab4514e8128
Do Le Duy <>
2020-05-10 15:41:25 +0700

    duydl - Thêm LauchScreen

--- ColorNinja/AppDelegate.swift
+++ ColorNinja/AppDelegate.swift
@@ -13,8 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window : UIWindow?
 
+    /* Đã load xong LaunchScreen */
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 1.0)
         return true
     }
 
