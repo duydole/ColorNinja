90db9b05441c4a0693aaeded66f27ce13cf030e3
Do Le Duy <>
2020-05-20 21:37:38 +0700

    duydl - Refactor lại ButtonWithImage

--- ColorNinja/Common/CustomButton/ButtonWithImage.swift
+++ ColorNinja/Common/CustomButton/ButtonWithImage.swift
@@ -10,13 +10,27 @@ import Foundation
 import UIKit
 
 class ButtonWithImage: UIView {
+        
+    var imageView: UIImageView!
     
-    // MARK: - Public property
+    var titleLabel: UILabel!
     
-    var image: UIImage?
+    var buttonPadding: UIEdgeInsets!
     
-    var titleText: String?
+    var spacing: CGFloat!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageView = UIImageView()
+        titleLabel = UILabel()
+        buttonPadding = UIEdgeInsets()
+        spacing = 0.0
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
     func addTargetForTouchUpInsideEvent(target: Any, selector: Selector) {
         let gesture:UITapGestureRecognizer = UITapGestureRecognizer(target: target, action: selector)
         gesture.numberOfTapsRequired = 1
@@ -24,45 +38,28 @@ class ButtonWithImage: UIView {
         addGestureRecognizer(gesture)
     }
     
-    // MARK: - Private property
-    
-    private var imageView: UIImageView!
-    
-    private var titleLabel: UILabel!
-    
-    // MARK: - Overrides
-    
     override func layoutSubviews() {
         
         let buttonHeight: CGFloat = self.frame.size.height
-        let padding: CGFloat = 10
-        let imageViewWidth: CGFloat = buttonHeight - padding*2
+        let imageViewWidth =  buttonHeight - buttonPadding.top - buttonPadding.bottom
         
         // image
-        imageView = UIImageView()
-        if let image = self.image {
-            imageView.image = image
-        }
         self.addSubview(imageView)
         imageView.snp.makeConstraints { (make) in
             make.width.equalTo(imageViewWidth)
-            make.leading.top.equalTo(padding)
-            make.bottom.equalTo(-padding)
+            make.top.equalTo(buttonPadding.top)
+            make.leading.equalTo(buttonPadding.left)
+            make.bottom.equalTo(-buttonPadding.bottom)
         }
         
         // title
-        titleLabel = UILabel()
-        titleLabel.textAlignment = .center
         titleLabel.textColor = .white
-        titleLabel.font = UIFont.systemFont(ofSize: 25)
-        if let text = self.titleText {
-            titleLabel.text = text
-        }
         self.addSubview(titleLabel)
         titleLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(padding)
-            make.leading.equalTo(imageView.snp.trailing).offset(padding)
-            make.trailing.bottom.equalTo(-padding)
+            make.top.equalTo(buttonPadding.top)
+            make.leading.equalTo(imageView.snp.trailing).offset(spacing)
+            make.bottom.equalTo(-buttonPadding.bottom)
+            make.trailing.equalTo(-buttonPadding.right)
         }
     }
 }
