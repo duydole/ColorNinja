f7a4f9ab8575ca3c6d2963acc289043778f9ae09
Do Le Duy <>
2020-05-27 00:04:03 +0700

    duydl - Refactor ColorStore

--- ColorNinja/Manager/ColorStore.swift
+++ ColorNinja/Manager/ColorStore.swift
@@ -10,97 +10,24 @@ import Foundation
 import UIKit
 
 class ColorStore {
-    
-    // MARK: - Shared
-    
+        
     static let shared = ColorStore()
     
-    // MARK: - Public Property
+    // MARK: Public Property
     
-    var allColors: [UIColor]!
+    public private(set) var allColors: [UIColor]!
     
-    // MARK: - Public Methods
+    // MARK: Public Methods
     
     func getPairOfColorRandom(levelOfDifficult: Int) -> (UIColor, UIColor) {
         let randomColor = self.allColors[Int.random(in: 0...allColors.count - 1)]
-        let samesameColor = self.generateRandomSameSameColor(rootColot: randomColor, level: levelOfDifficult)
+        let samesameColor = self.generateRandomSameSameColor(rootColor: randomColor, level: levelOfDifficult)
         return (randomColor, samesameColor)
     }
     
-    // MARK: - Private
-    
-    private init() {
-        self.setupColors()
-    }
-    
-    private func setupColors() {
-        allColors = [
-            ColorRGB(8, 170, 145),
-            ColorRGB(238, 93, 39),
-            ColorRGB(223, 27, 83),
-            ColorRGB(118, 41, 140),
-            ColorRGB(84, 79, 136),
-            ColorRGB(144, 11, 62),
-            ColorRGB(119, 1, 56),
-            ColorRGB(255, 87, 51),
-            ColorRGB(255, 195, 3),
-            ColorRGB(67, 41, 103),
-            ColorRGB(120, 45, 101),
-            ColorRGB(235, 219, 152),
-            ColorRGB(249, 66, 82),
-            ColorRGB(32, 68, 91),
-            ColorRGB(123, 56, 65),
-            ColorRGB(248, 231, 169),
-            ColorRGB(197, 225, 111),
-            ColorRGB(196, 55, 129),
-            ColorRGB(70, 93, 110),
-            ColorRGB(240, 192, 168)
-        ]
-    }
-    
-    // Dùng cho chơi Offline
-    func generateRandomSameSameColor(rootColot: UIColor, level: Int) -> UIColor {
-        
-        // Get RGB value
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        var alpha: CGFloat = 0
-        rootColot.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        red = red*255
-        green = green*255
-        blue = blue*255
-        
-        // Random độ thay đổi
-        var degreeOfChange : CGFloat = 0
-        if level < 5 {
-            degreeOfChange = 50
-        } else if level < 10 {
-            degreeOfChange = 40
-        } else if level < 15 {
-            degreeOfChange = 35
-        } else if level < 20 {
-            degreeOfChange = 30
-        } else if level < 20 {
-            degreeOfChange = 25
-        } else {
-            degreeOfChange = 20
-        }
-        
-        // Random đổi R, G hay B
+    func generateRandomSameSameColor(rootColor: UIColor, level: Int) -> UIColor {
         let colorWillChange = Int.random(in: 0...2)
-        if colorWillChange == 0 {
-            red = red + degreeOfChange > 255 ? 255 : red + degreeOfChange
-            green = green + degreeOfChange > 255 ? 255 : green + degreeOfChange
-        } else if colorWillChange == 1 {
-            green = green + degreeOfChange > 255 ? 255 : green + degreeOfChange
-            blue = blue + degreeOfChange > 255 ? 255 : blue + degreeOfChange
-        } else if colorWillChange == 2 {
-            blue = blue + degreeOfChange > 255 ? 255 : blue + degreeOfChange
-            red = red + degreeOfChange > 255 ? 255 : red + degreeOfChange
-        }
-        
-        return ColorRGB(red, green, blue)
+        return generateRandomSameSameColor(rootColot: rootColor, level: level, RGBIndexWillBeChanged: colorWillChange)
     }
     
     func generateRandomSameSameColor(rootColot: UIColor, level: Int, RGBIndexWillBeChanged: Int) -> UIColor {
@@ -144,6 +71,37 @@ class ColorStore {
         }
         
         return ColorRGB(red, green, blue)
+    }
+
+    // MARK: Private
+    
+    private init() {
+        self.setupColors()
+    }
+    
+    private func setupColors() {
+        allColors = [
+            ColorRGB(8, 170, 145),
+            ColorRGB(238, 93, 39),
+            ColorRGB(223, 27, 83),
+            ColorRGB(118, 41, 140),
+            ColorRGB(84, 79, 136),
+            ColorRGB(144, 11, 62),
+            ColorRGB(119, 1, 56),
+            ColorRGB(255, 87, 51),
+            ColorRGB(255, 195, 3),
+            ColorRGB(67, 41, 103),
+            ColorRGB(120, 45, 101),
+            ColorRGB(235, 219, 152),
+            ColorRGB(249, 66, 82),
+            ColorRGB(32, 68, 91),
+            ColorRGB(123, 56, 65),
+            ColorRGB(248, 231, 169),
+            ColorRGB(197, 225, 111),
+            ColorRGB(196, 55, 129),
+            ColorRGB(70, 93, 110),
+            ColorRGB(240, 192, 168)
+        ]
     }
 }
 
